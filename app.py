e9400eafe6a713969d66fa1f193c6c1efecfe947
Sang Vang <vangsang2019@gmail.com>
2024-03-28 14:20:49 -0700

    Setting up how the homepage may look like

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+# Dummy Data
+posts = [
+    {
+
+        'author' : 'Sangy Vangy',
+        'title' : 'Blog Post 1',
+        'content' : 'This is for test purposes only',
+        'date_posted' : 'July 3, 2024'
+
+    },
+    {
+
+        'author' : 'John Doe',
+        'title' : 'Happy Independence Day!',
+        'content' : 'Today we are outside prepping for the fireworks show but I was wondering if there was a way to make this into a program. Any thoughts?',
+        'date_posted' : 'July 4, 2024'
+
+    },
+]
 
 @app.get('/')
 def home():
 
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
