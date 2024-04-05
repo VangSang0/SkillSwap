c567ee62026cf21c3f8b2d018f4852d652821ac3
Sang Vang <vangsang2019@gmail.com>
2024-04-05 10:33:42 -0700

    Adding Sang's features into the main because of merge complications

--- app.py
+++ app.py
@@ -3,9 +3,34 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 
+# Dummy Data
+posts = [
+    {
+
+        'author' : 'Sangy Vangy',
+        'title' : 'Post 1',
+        'content' : 'This is for test purposes only',
+        'date_posted' : 'July 3, 2024'
+
+    },
+    {
+
+        'author' : 'John Doe',
+        'title' : 'Happy Independence Day!',
+        'content' : 'Today we are outside prepping for the fireworks show but I was wondering if there was a way to make this into a program. Any thoughts?',
+        'date_posted' : 'July 4, 2024'
+
+    },
+]
+
 @app.get('/')
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.get('/home')
 def home():
-    return render_template('index.html')
+
+    return render_template('index.html', posts=posts)
 
 
 @app.get('/profile')
