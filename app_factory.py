631308f7e8bfb9923a6c0335071d4d6eaa6699d3
Sang Vang <vangsang2019@gmail.com>
2024-05-02 09:58:56 -0700

    reverting it back to original

--- app_factory.py
+++ app_factory.py
@@ -2,9 +2,9 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = os.getenv('SECRET_KEY')
-    bcrypt = Bcrypt(app)
-    app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
-    return app, bcrypt
+# def create_app():
+#     app = Flask(__name__)
+#     app.secret_key = os.getenv('SECRET_KEY')
+#     bcrypt = Bcrypt(app)
+#     app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+#     return app, bcrypt
